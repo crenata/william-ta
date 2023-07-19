b16b176135aa7e72afee15dde2fdbf00c0933139
williamimmanuel25@gmail.com <williamimmanuel25@gmail.com>
2023-07-19 10:52:32 +0700

    message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -146,10 +146,10 @@
                                             <img src="{{ $product->images[0]->image }}" class="card-img-top"
                                                  style="max-height: 200px" alt="{{ $product->name }}">
                                             <div class="card-body">
-                                                <h4 class="card-title">{{ $product->name }}</h4>
-                                                <h5 class="m-0 {{ empty($product->offer_price) ? "" : "text-decoration-line-through" }}">
+                                                <h4 class="card-title text-center">{{ $product->name }}</h4>
+                                                <h5 class="text center {{ empty($product->offer_price) ? "" : "text-decoration-line-through" }}">
                                                     Rp {{ number_format($product->price) }}</h5>
-                                                <h4 class="m-0 {{ empty($product->offer_price) ? "d-none" : "" }}">
+                                                <h4 class="text-center {{ empty($product->offer_price) ? "d-none" : "" }}">
                                                     Rp {{ number_format($product->offer_price) }}</h4>
                                             </div>
                                         </a>
